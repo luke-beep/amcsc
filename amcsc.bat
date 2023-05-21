ed09545e48d4a2fcf24e970b21de95a79ed8f695
LukeHjo <lukehjo@gmail.com>
2023-05-21 15:25:43 +0200

    E.

--- amcsc.bat
+++ amcsc.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set /p folderlocation="Enter the file location where you want to create a folder: "
-set /p foldername="Enter the name of the folder you want to create: "
+set /p folderlocation="Where do you want to create the folder? (Example: C:\Users\%username%\Desktop) "
+set /p foldername="What do you want the folder to be called? "
 md "!folderlocation!\!foldername!"
 cd "!folderlocation!\!foldername!"
 
-set /p memvalue="Enter the memory value: "
+set /p memvalue="How much memory do you want to allocate? (In MB): "
 
 > "eula.txt" (
     echo #By changing the setting below to TRUE you are indicating your agreement to our EULA ^(https://aka.ms/MinecraftEULA^).
@@ -18,25 +18,25 @@ echo The following server launchers are available:
 echo 1. Vanilla (1.19.4 - 1.7.4)
 echo 2. Mohist (1.19.4 - 1.7.10)
 echo 3. Fabric (1.19.4 - 1.14)
-echo 4. Forge | WIP (1.19.4 - 1.7.10)
-echo 5. Catserver | WIP
-echo 6. Waterfall | WIP
-echo 7. Bungeecord | WIP
-echo 8. Velocity | WIP
-echo 9. Paper | WIP
-echo 10. Purpur | WIP
-echo 11. Sponge | WIP
+echo 4. Forge WIP (1.19.4 - 1.7.10)
+echo 5. Catserver  WIP
+echo 6. Waterfall  WIP
+echo 7. Bungeecord WIP
+echo 8. Velocity WIP
+echo 9. Paper WIP
+echo 10. Purpur WIP
+echo 11. Sponge WIP
 set /p launcher=What server launcher do you want to install? Pick between 1 - 11.
 
 if !launcher! == 1 (
     echo java -Xmx!memvalue!M -Xms!memvalue!M -jar server.jar nogui > start.bat
     echo You have chosen Vanilla.
-    echo The following versions are available:
+    echo The following versions are available: 
     set "vanillaversions=1.19.4 1.19.3 1.19.2 1.19.1 1.19 1.18.2 1.18.1 1.18 1.17.2 1.17.1 1.17 1.16.5 1.16.4 1.16.3 1.16.2 1.16.1 1.16 1.15.2 1.15.1 1.15 1.14.4 1.14.3 1.14.2 1.14.1 1.14 1.13.2 1.13.1 1.13 1.12.2 1.12.1 1.12 1.11.2 1.11.1 1.11 1.10.2 1.10.1 1.10 1.9.4 1.9.3 1.9.2 1.9.1 1.9 1.8.9 1.8.8 1.8.7 1.8.6 1.8.5 1.8.4 1.8.3 1.8.2 1.8.1 1.8 1.7.10 1.7.9 1.7.8 1.7.7 1.7.6 1.7.5 1.7.4"
     for %%v in (!vanillaversions!) do (
         echo %%v
     )
-    set /p vanillaversion=What version do you want to install?
+    set /p vanillaversion=What version do you want to install? (Example: 1.17.1) 
     set vanillaurl=https://serverjars.com/api/fetchJar/vanilla/vanilla/!vanillaversion!
     powershell -Command "(New-Object Net.WebClient).DownloadFile('!vanillaurl!', 'server.jar')"
     powershell -Command "Invoke-WebRequest !vanillaurl! -OutFile server.jar"
@@ -52,7 +52,7 @@ if !launcher! == 2 (
     for %%v in (!mohistversions!) do (
         echo %%v
     )
-    set /p mohistversion=What version do you want to install?
+    set /p mohistversion=What version do you want to install? (Example: 1.12.2) 
     set mohisturl=https://serverjars.com/api/fetchJar/modded/mohist/!mohistversion!
     powershell -Command "(New-Object Net.WebClient).DownloadFile('!mohisturl!', 'server.jar')"
     powershell -Command "Invoke-WebRequest !mohisturl! -OutFile server.jar"
@@ -68,7 +68,7 @@ if !launcher! == 3 (
     for %%v in (!fabricversions!) do (
         echo %%v
     )
-    set /p fabricversion=What version do you want to install?
+    set /p fabricversion=What version do you want to install? (Example: 1.17.1) 
     set fabricurl=https://serverjars.com/api/fetchJar/modded/fabric/!fabricversion!
     powershell -Command "(New-Object Net.WebClient).DownloadFile('!fabricurl!', 'server.jar')"
     powershell -Command "Invoke-WebRequest !fabricurl! -OutFile server.jar"
@@ -84,12 +84,21 @@ if !launcher! == 4 (
     for %%v in (!forgeversions!) do (
         echo %%v
     )
-    set /p forgeversion=What version do you want to install?
+    set /p forgeversion=What version do you want to install? (Example: 1.12.2) 
     set forgeurl=https://serverjars.com/api/fetchJar/modded/forge/!forgeversion!
     powershell -Command "(New-Object Net.WebClient).DownloadFile('!forgeurl!', 'server.jar')"
     powershell -Command "Invoke-WebRequest !forgeurl! -OutFile server.jar"
     START "" start.bat
-    exit
+    echo Installing Forge and setting everything up. Wait 60 seconds...
+    timeout /t 60 > nul
+    if exist "run.bat" (
+    call run.bat
+    ) else (
+    for /f "delims=" %%F in ('dir /b minecraft_server*') do (
+        call "%%F"'
+        exit /b
+    )
+    )
 )
 
 
